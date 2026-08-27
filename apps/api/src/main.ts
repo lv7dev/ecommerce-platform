fce0fa24699e3161cba8e77c90842856c99c6fdd
lv7dev <tuanluong0704@gmail.com>
2026-08-27 16:15:57 +0700

    feat(api): add Swagger documentation setup and update environment variables

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,20 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  if (process.env.NODE_ENV !== 'production') {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('E-commerce API')
+      .setDescription('API documentation for the E-commerce Platform')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const documentFactory = () =>
+      SwaggerModule.createDocument(app, swaggerConfig);
+
+    SwaggerModule.setup('api/docs', app, documentFactory);
+  }
 
   const port = Number(process.env.PORT ?? 4000);
 
